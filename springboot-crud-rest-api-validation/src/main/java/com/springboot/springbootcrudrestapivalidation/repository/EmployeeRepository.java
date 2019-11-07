2ecab19f9c202879d516ceb9ce03cd8dad05ee91
GeethaBadisi <57406415+GeethaBadisi@users.noreply.github.com>
2019-11-07 21:41:51 +0530

    Add files via upload

---
+++ springboot-crud-rest-api-validation/src/main/java/com/springboot/springbootcrudrestapivalidation/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+	package com.springboot.springbootcrudrestapivalidation.repository;
+	
+	import org.springframework.data.jpa.repository.JpaRepository;
+	import org.springframework.stereotype.Repository;
+
+import com.springboot.springbootcrudrestapivalidation.model.Employee;
+	
+	@Repository
+	public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+	
+	}
---
+++ springboot-crud-rest-api-validation/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO EMPLOYEES (first_name, last_name, email_address) VALUES
+  ('Lokesh', 'Gupta', 'abc@gmail.com'),
+  ('Deja', 'Vu', 'xyz@email.com'),
+  ('Caption', 'America', 'cap@marvel.com');
